310a99457f1382dd39c2e441d96fa6cc335528b0
Terje Engelbertsen <terje.engelbertsen@swarco.com>
2019-11-08 17:03:57 +0100

    Elastic working.

--- Sigma/Controllers/ElasticSearchController.cs
+++ Sigma/Controllers/ElasticSearchController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nest;
+using Sigma.Backgroundservices;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,15 +16,25 @@ namespace Sigma.Controllers
     public class ElasticSearchController : ControllerBase
     {
         private readonly ILogger<ElasticSearchController> _logger;
+        private readonly IElasticClient _elasticClient;
+        private readonly RetrieveSensorDataClient _retrieveSensorDataClient;
 
-        public ElasticSearchController(ILogger<ElasticSearchController> logger)
+        public ElasticSearchController(ILogger<ElasticSearchController> logger, 
+                                       IElasticClient elasticClient, 
+                                       RetrieveSensorDataClient retrieveSensorDataClient)
         {
             _logger = logger;
+            _retrieveSensorDataClient = retrieveSensorDataClient;
+            _elasticClient = elasticClient;
         }
 
         // GET: /<controller>/
         public async Task<ActionResult> Index()
         {
+            //await _elasticClient.UpdateAsync<SensorDataModels.SensorMetaData>(post, u => u.Doc(post));
+
+            await _elasticClient.IndexDocumentAsync(_retrieveSensorDataClient._metaSensorData[0]);
+
             return Ok();
         }
     }
